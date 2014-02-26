f880382731510e6139079bce1850fe0052413c0c
swordmaster2k <b00056835@student.itb.ie>
2014-02-26 17:57:57 +0000

    + Added an opacity slider to the layers frame. Still need to hook up the change listener.

--- src/rpgtoolkit/editor/board/LayerFrame.java
+++ src/rpgtoolkit/editor/board/LayerFrame.java
@@ -1,8 +1,16 @@
 package rpgtoolkit.editor.board;
 
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSlider;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import rpgtoolkit.editor.utilities.LayerTableModel;
@@ -16,8 +24,13 @@ public class LayerFrame extends JInternalFrame
 {
     private AbstractBoardView boardView;
     
+    private JSlider opacitySlider;
+    private JLabel opacitySliderLabel;
     private JTable layerTable;
     private JScrollPane layerScrollPane;
+    
+    private JPanel sliderPanel;
+    private JPanel layerPanel;
     private JPanel contentPanel;
     
     public LayerFrame()
@@ -35,6 +48,21 @@ public class LayerFrame extends JInternalFrame
     
     private void initialize()
     {
+        this.opacitySlider = new JSlider(0, 100, 100);
+        //this.opacitySlider.addChangeListener(this);
+        
+        this.opacitySliderLabel = new JLabel("Opacity");
+        this.opacitySliderLabel.setLabelFor(this.opacitySlider);
+        
+        this.sliderPanel = new JPanel();
+        this.sliderPanel.setLayout(new BoxLayout(sliderPanel, BoxLayout.X_AXIS));
+        this.sliderPanel.setBorder(BorderFactory.createEmptyBorder(0, 3, 0, 3));
+        this.sliderPanel.add(this.opacitySliderLabel);
+        this.sliderPanel.add(this.opacitySlider);
+        this.sliderPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE,
+                                            this.sliderPanel.getPreferredSize()
+                                                    .height));
+        
         this.layerTable = new JTable(new LayerTableModel(this.boardView));
         this.layerTable.getColumnModel().getColumn(0).setPreferredWidth(32);
         this.layerTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -42,8 +70,29 @@ public class LayerFrame extends JInternalFrame
         
         this.layerScrollPane = new JScrollPane(this.layerTable);
         
+        this.layerPanel = new JPanel();
+        this.layerPanel.setLayout(new GridBagLayout());
+        //this.layerPanel.setPreferredSize(new Dimension(120, 120));
+        
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.insets = new Insets(3, 0, 0, 0);
+        constraints.weightx = 1;
+        constraints.weighty = 0;
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.weighty = 0;
+        constraints.gridy++;
+        this.layerPanel.add(this.sliderPanel, constraints);
+        constraints.weighty = 1;
+        constraints.gridy++;
+        this.layerPanel.add(layerScrollPane, constraints);
+        constraints.weighty = 0;
+        constraints.insets = new Insets(0, 0, 0, 0);
+        constraints.gridy++;
+        
         this.contentPanel = new JPanel();
-        this.contentPanel.add(this.layerScrollPane);
+        this.contentPanel.add(this.layerPanel);
         
         this.setContentPane(this.contentPanel);
         this.pack();
