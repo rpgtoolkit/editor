202adc45a34a2774c08c572e70a0ea284ca4cdeb
Chris Hutchinson <chris@cshutchinson.com>
2015-06-09 02:41:34 -0400

    Added project license. Added copyright boilerplate. Touched nearly every file in the repository; however, merge should be painless because all of them were prepended.

--- common/src/main/java/net/rpgtoolkit/common/assets/Asset.java
+++ common/src/main/java/net/rpgtoolkit/common/assets/Asset.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2015, rpgtoolkit.net <help@rpgtoolkit.net>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 /// See LICENSE.md in the distribution for the full license text including,
 /// but not limited to, a notice of warranty and distribution rights.
 
