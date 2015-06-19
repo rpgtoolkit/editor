fa9b64f558361082934ca26f0f45ba4a343b1faf
swordmaster2k <b00056835@student.itb.ie>
2015-06-19 08:55:51 +0100

    + Added ProgramPanel skeleton to project.

---
+++ editor/src/main/java/net/rpgtoolkit/editor/ui/BoardProgramPanel.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2015, rpgtoolkit.net <help@rpgtoolkit.net>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package net.rpgtoolkit.editor.ui;
+
+import javax.swing.JComboBox;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
+import net.rpgtoolkit.common.assets.BoardProgram;
+
+/**
+ *
+ * @author Joshua Michael Daly
+ */
+public class BoardProgramPanel extends AbstractModelPanel
+{
+    private JTextField fileTextField;
+    private JSpinner layerSpinner;
+    private JComboBox activationComboBox;
+    private JSpinner repeatSpinner;
+
+    /*
+     * *************************************************************************
+     * Public Constructors
+     * *************************************************************************
+     */
+    public BoardProgramPanel(BoardProgram boardProgram)
+    {
+        ///
+        /// super
+        ///
+        super(boardProgram);
+        ///
+        ///
+        ///
+        
+        ///
+        ///this
+        ///
+    }
+    
+}
